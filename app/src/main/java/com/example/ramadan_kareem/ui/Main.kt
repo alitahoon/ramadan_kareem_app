ce1979694db3159ee4a1c44d0ca908076e838bd5
Ali Tahoon <73113143+alitahoon@users.noreply.github.com>
2023-03-17 16:50:18 +0200

    create project fragments _finish repo di module & network module

---
+++ app/src/main/java/com/example/ramadan_kareem/ui/Main.kt
@@ -0,0 +1,78 @@
+package com.example.ramadan_kareem.ui
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import com.ashokvarma.bottomnavigation.BottomNavigationBar
+import com.ashokvarma.bottomnavigation.BottomNavigationItem
+import com.example.ramadan_kareem.R
+import com.example.ramadan_kareem.databinding.ActivityMainBinding
+
+
+class Main : AppCompatActivity() {
+    private val TAG:String?="Main"
+    private lateinit var binding:ActivityMainBinding
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        hideActionBar()
+        setBottomBarIcons()
+        binding.bottomNavigationBar.setTabSelectedListener(object :
+            BottomNavigationBar.OnTabSelectedListener {
+            override fun onTabSelected(position: Int) {
+                Log.i(TAG,position.toString())
+            }
+            override fun onTabUnselected(position: Int) {
+
+            }
+            override fun onTabReselected(position: Int) {
+
+            }
+        })
+    }
+
+    private fun hideActionBar() {
+        if (getSupportActionBar() != null) {
+            try {
+                this.supportActionBar!!.hide()
+            } // catch block to handle NullPointerException
+            catch (e: NullPointerException) {
+                Log.e(TAG,e.message.toString())
+            }
+        }
+    }
+
+    private fun setBottomBarIcons() {
+        binding.bottomNavigationBar.setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_STATIC)
+        binding.bottomNavigationBar
+            .addItem(BottomNavigationItem(R.drawable.home, "الرئيسيه")).setActiveColor(R.color.homeCardViewIconColorDark)
+            .addItem(BottomNavigationItem(R.drawable.search, "البحث")).setActiveColor(R.color.homeCardViewIconColorDark)
+            .addItem(BottomNavigationItem(R.drawable.favourite, "المفضله")).setActiveColor(R.color.homeCardViewIconColorDark)
+            .addItem(BottomNavigationItem(R.drawable.calender, "التقويم")).setActiveColor(R.color.homeCardViewIconColorDark)
+            .setFirstSelectedPosition(0)
+            .initialise()
+    }
+//    private fun setupSmoothBottomMenu() {
+//        binding.bottomBar.setupWithNavController(navController)
+//    }
+
+    //set an active fragment programmatically
+//    fun setSelectedItem(pos:Int){
+//        binding.bottomBar.setSelectedItem(pos)
+//    }
+//    //set badge indicator
+//    fun setBadge(pos:Int){
+//        binding.bottomBar.setBadge(pos)
+//    }
+//    //remove badge indicator
+//    fun removeBadge(pos:Int){
+//        binding.bottomBar.removeBadge(pos)
+//    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
